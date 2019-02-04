11f32ab16e520a936ed3c9f8cb4fe013cc00ce3a
coque <coquejardon@gmail.com>
2019-02-04 17:52:37 +0100

    Practica 2A - Ejercicio 01
    Holi

--- app/src/main/java/fyq/example/labdadm/labs/DashboardActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/DashboardActivity.java
@@ -2,12 +2,17 @@ package fyq.example.labdadm.labs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-//Esta es la branch de pruebas ferran
+public class DashboardActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void goTo(View view){
+
     }
 }
