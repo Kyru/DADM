d7e0f79e8be0a8ff1692d307c2338bac40efd141
Unknown <ferraniusd@gmail.com>
2019-02-04 15:57:33 +0100

    Added Android Studio

---
+++ app/src/main/java/fyq/example/labdadm/labs/MainActivity.java
@@ -0,0 +1,13 @@
+package fyq.example.labdadm.labs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Prueba Ferran 2
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
