efb950af4159974021b567f2d868c974bd8a527d
coque <coquejardon@gmail.com>
2019-02-04 16:04:20 +0100

    Prueba 1.01

--- app/src/main/java/fyq/example/labdadm/labs/MainActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Prueba Ferran 2
+//Prueba Ferran 2 Coque 3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
