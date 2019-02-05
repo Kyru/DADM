62cb54d712fb6e09c3e5b83a509482d4bb5d1f6d
coque <coquejardon@gmail.com>
2019-02-05 15:49:15 +0100

    Practica 2A - Ejercicio 02

--- app/src/main/java/fyq/example/labdadm/labs/DashboardActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/DashboardActivity.java
@@ -1,8 +1,10 @@
 package fyq.example.labdadm.labs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     public void goTo(View view){
-
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
