b42bf68dd3375b2e937ab10775beb3b87d7367dc
Unknown <ferraniusd@gmail.com>
2019-02-05 16:31:14 +0100

    Practica 02 - Ejercicio 5 - empezado
    He empezado a añadir cosas al layout

---
+++ app/src/main/java/fyq/example/labdadm/labs/QuotationActivity.java
@@ -0,0 +1,13 @@
+package fyq.example.labdadm.labs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuotationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotation);
+    }
+}
