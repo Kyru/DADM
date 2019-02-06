c00e76efbc0ac6c2acbc356b8846cee72a4646d3
coque <coquejardon@gmail.com>
2019-02-06 18:49:43 +0100

    2A_8 completed
    PRACTICA 2A c'est fini

--- app/src/main/java/fyq/example/labdadm/labs/DashboardActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/DashboardActivity.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import java.util.Queue;
 
 public class DashboardActivity extends AppCompatActivity {
 
