d732b6231383711da16d7a23a3e5f1f73e42c2d9
Unknown <ferraniusd@gmail.com>
2019-02-08 16:00:44 +0100

    2B_3 completed

--- app/src/main/java/fyq/example/labdadm/labs/FavouriteActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/FavouriteActivity.java
@@ -2,11 +2,17 @@ package fyq.example.labdadm.labs;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 import fyq.example.labdadm.labs.QuotationMethods.QuotationArrayAdapter;
@@ -25,28 +31,55 @@ public class FavouriteActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.lv_quotations);
         listView.setAdapter(quotationArrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Quotation quotation = (Quotation) parent.getItemAtPosition(position);
+                String quotationAuthor = quotation.getQuoteAuthor();
+
+                if (quotationAuthor.equals("") || quotationAuthor == null) {
+                    Toast.makeText(FavouriteActivity.this,
+                            "No se ha podido obtener la información", Toast.LENGTH_LONG).show();
+                } else {
+                    findAuthor(view, quotationAuthor);
+                }
+
+
+            }
+        });
+
     }
 
-    public void findAuthor(View view){
+    public void findAuthor(View view, String authorName) {
 
-        String authorName = "Albert Einstein";
+        try {
+            String authorNameEncoded = URLEncoder.encode(authorName, "UTF-8");
 
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("https://en.wikipedia.org/wiki/Special:Search?search=" + authorName));
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://en.wikipedia.org/wiki/Special:Search?search=" + authorNameEncoded));
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+
+        } catch (Exception e) {
+            Log.d("Yourapp", "UnsupportedEncodingException");
         }
     }
 
-    public ArrayList<Quotation> getMockQuotations(){
+    public ArrayList<Quotation> getMockQuotations() {
 
         ArrayList<Quotation> mockListQuotation = new ArrayList<>();
-        Quotation mockQuotation = new Quotation("This is a mock quotation", "F");
-        for (int i = 0; i < 10; i++){
+        Quotation mockQuotation = new Quotation("if you want to impress someone put him on your black list",
+                "Johnnie Walker");
+        for (int i = 0; i < 10; i++) {
             mockListQuotation.add(mockQuotation);
         }
+
+        Quotation nullQuotation = new Quotation("This is a null quotation", "");
+        mockListQuotation.add(nullQuotation);
+
         return mockListQuotation;
     }
 
