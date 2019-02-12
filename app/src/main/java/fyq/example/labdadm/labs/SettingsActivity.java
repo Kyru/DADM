217e713565b5740783dc8ded7f64750b6b1ef9b6
coque <coquejardon@gmail.com>
2019-02-12 16:30:12 +0100

    3.2 half

--- app/src/main/java/fyq/example/labdadm/labs/SettingsActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/SettingsActivity.java
@@ -1,42 +1,44 @@
 package fyq.example.labdadm.labs;
 
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.List;
 
-    EditText editTextName;
+/**
+ * A {@link PreferenceActivity} that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
+ */
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        editTextName = findViewById(R.id.editText_Name);
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        editTextName.setText(prefs.getString("name", ""));
-    }
-
-    @Override
-    protected void onPause() {
-        SharedPreferences prefs =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = prefs.edit();
-
-
-
-        if(editTextName.getText().toString().equals("") || editTextName.getText().toString() == null ){
-            editor.remove("name");
-        } else {
-            editor.putString("name", editTextName.getText().toString());
-        }
-
-        editor.apply();
-        super.onPause();
+        addPreferencesFromResource(R.xml.preferences_settings);
     }
 
 
