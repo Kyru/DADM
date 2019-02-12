0d6eece40e7cfed5dd4e262743294d10e5cb5956
Unknown <ferraniusd@gmail.com>
2019-02-12 15:30:39 +0100

    3-1 started

--- app/src/main/java/fyq/example/labdadm/labs/SettingsActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/SettingsActivity.java
@@ -1,7 +1,10 @@
 package fyq.example.labdadm.labs;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,4 +13,23 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    @Override
+    protected void onPause() {
+        SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        EditText editTextName = findViewById(R.id.editText_Name);
+        if(editTextName.getText().toString().equals("") || editTextName.getText().toString() == null ){
+            editor.remove("name");
+        } else {
+            editor.putString("name", editTextName.getText().toString());
+        }
+
+        editor.apply();
+        super.onPause();
+    }
+
+
 }
