5ededd47c9544265f090be3c956b741f4034c549
coque <coquejardon@gmail.com>
2019-02-12 15:31:17 +0100

    3-1 gana el betis

--- app/src/main/java/fyq/example/labdadm/labs/SettingsActivity.java
+++ app/src/main/java/fyq/example/labdadm/labs/SettingsActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
